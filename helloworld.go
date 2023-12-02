5defea80863332d8d349d452a6196f1b0815f3aa
Alif Jian <alifjian834@gmail.com>
2023-12-02 15:12:07 +0700

    Belajar tipe data dan VAR

---
+++ helloworld.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	type nama string       //inisialisasi tipe data dengan nama baru
+	var user nama = "Asep" //membuat variable asep dengan tipe data nama di isi dengan Asep
+	fmt.Println("Hello", user)
+}
