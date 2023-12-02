80e4b9afe3960169f4f56a16ce1c825fa74a4257
Alif Jian <alifjian834@gmail.com>
2023-12-02 15:17:23 +0700

    Menambahkan line untuk mengganti user lalu tampilkan ulang

--- helloworld.go
+++ helloworld.go
@@ -6,4 +6,6 @@ func main() {
 	type nama string       //inisialisasi tipe data dengan nama baru
 	var user nama = "Asep" //membuat variable asep dengan tipe data nama di isi dengan Asep
 	fmt.Println("Hello", user)
+	user = "Sahrul"
+	fmt.Println("Hello", user)
 }
