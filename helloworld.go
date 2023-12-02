45257dbb7d97cbcca43787aeb3282c1061b3cc93
Alif Jian <alifjian834@gmail.com>
2023-12-02 15:15:04 +0700

    Membuat variabel konstanta supaya nama user tidak dapat di ubah

--- helloworld.go
+++ helloworld.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	type nama string       //inisialisasi tipe data dengan nama baru
-	var user nama = "Asep" //membuat variable asep dengan tipe data nama di isi dengan Asep
+	type nama string         //inisialisasi tipe data dengan nama baru
+	const user nama = "Asep" //membuat variable konstanta dimna variable ini tidak bisa diubah asep dengan tipe data nama di isi dengan Asep
 	fmt.Println("Hello", user)
 }
