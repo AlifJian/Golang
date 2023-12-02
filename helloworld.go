f110d9c0eed011ae798d2c5eff833cf2efa2fbdd
Alif Jian <alifjian834@gmail.com>
2023-12-02 15:21:58 +0700

    Menghapus perubahan sebelumnya

--- helloworld.go
+++ helloworld.go
@@ -6,6 +6,6 @@ func main() {
 	type nama string         //inisialisasi tipe data dengan nama baru
 	const user nama = "Asep" //membuat variable konstanta dimna variable ini tidak bisa diubah asep dengan tipe data nama di isi dengan Asep
 	fmt.Println("Hello", user)
-	user = "Sahrul"
-	fmt.Println("Hello", user)
+	// Menghapus perubahan sebelumnya karena setelah di merge dengan branch development variable user menjadi konstanta dan tidak bisa di ubah
+
 }
