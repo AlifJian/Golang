b9cbd06b64a6a266bf4aad09e4dd76b58a14b078
Alif Jian <alifjian834@gmail.com>
2024-01-27 20:56:19 +0700

    Tugas TLX TOKI

---
+++ Main.go
@@ -0,0 +1,86 @@
+package main
+
+func main() {
+	// // Tugas 01
+	// fmt.Print("Halo, dunia!")
+
+	// // Tugas 03
+	// var A, B int
+
+	// fmt.Scan(&A, &B)
+	// fmt.Print(A + B)
+
+	// // Tugas 04
+	// var angka int32
+
+	// fmt.Scan(&angka)
+	// if angka > 0 {
+	// 	fmt.Print(angka)
+	// }
+
+	// // Tugas 06
+	// var kandang, jumlahBebek, i int32
+
+	// fmt.Scan(&kandang)
+	// for i = 0; i < kandang; i++ {
+	// 	var bebek int32
+	// 	fmt.Scan(&bebek)
+	// 	jumlahBebek += bebek
+	// }
+
+	// fmt.Print(jumlahBebek)
+
+	// // Tugas 07
+	// var loop, j int8
+
+	// fmt.Scan(&loop)
+	// for j = 1; j <= loop; j++ {
+	// 	if i%10 == 0 {
+	// 		continue
+	// 	}
+	// 	if i == 42 {
+	// 		fmt.Print("ERROR")
+	// 		break
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// Tugas 08
+	// var loop, i int16
+	// var angka, j int32
+	// fmt.Scan(&loop)
+	// for i = 0; i < loop; i++ {
+	// 	fmt.Scan(&angka)
+	// 	var checker bool = true
+	// 	for j = 2; j < angka && j < 999; j++ {
+	// 		if angka%j == 0 {
+	// 			checker = false
+	// 			break
+	// 		}
+	// 	}
+	// 	if checker && angka != 1 {
+	// 		fmt.Println("YA")
+	// 	} else {
+	// 		fmt.Println("BUKAN")
+	// 	}
+	// }
+
+	// Tugas 09
+	// var loop, i , maks int32
+	// var banyak int16
+	// var array = []int16{}
+	// banyak = 0
+	// maks = 0
+	// fmt.Scan(&loop)
+	// for i = 0; i < loop; i++ {
+	// 	var angka int16
+	// 	fmt.Scan(&angka)
+	// 	if banyak == 0 {
+	// 		banyak = angka
+	// 	}
+	// 	array = append(array, angka)
+	// }
+	// for i = 0; i < loop; i++ {
+	// 	fmt.Print(array[i])
+	// }
+}
--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,8 @@
-package main
+package hello
 
 import "fmt"
 
-func main() {
+func hello() {
 	type nama string         //inisialisasi tipe data dengan nama baru
 	const user nama = "Asep" //membuat variable konstanta dimna variable ini tidak bisa diubah asep dengan tipe data nama di isi dengan Asep
 	fmt.Println("Hello", user)
