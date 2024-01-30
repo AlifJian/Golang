b8d16c835bd4466bceafc7f9aa73e6d51cef2c3b
Alif Jian <alifjian834@gmail.com>
2024-01-30 17:53:37 +0700

    Tugas selesai)

--- Main.go
+++ Main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// // Tugas 01
 	// fmt.Print("Halo, dunia!")
@@ -83,4 +85,76 @@ func main() {
 	// for i = 0; i < loop; i++ {
 	// 	fmt.Print(array[i])
 	// }
+
+	// Tugas 10
+	// var A, B, C int32
+	// fmt.Scan(&A, &B)
+	// C = reverse(reverse(A) + reverse(B))
+	// fmt.Print(C)
+
+	// Tugas 11
+	// var S, T string
+	// fmt.Scan(&S, &T)
+
+	// for strings.Contains(S, T) {
+	// 	index := strings.Index(S, T)
+	// 	S = S[:index] + S[index+len(T):]
+	// }
+
+	// fmt.Println(S)
+
+	// Tugas 12 faktorial !!
+	// var N int32
+	// var hasil int32
+	// fmt.Scan(&N)
+
+	// hasil = faktor(N)
+	// fmt.Print(hasil)
+
+	// Tugas 13 gambar gunung
+	// var n int8
+	// fmt.Scan(&n)
+	// gambar(n)
+
+}
+
+func reverse(nilai int32) int32 {
+	var temp int32
+	var ret int32 = 0
+
+	for temp = nilai; temp > 0; temp = temp / 10 {
+		ret = (ret * 10) + (temp % 10)
+	}
+
+	return ret
+
+}
+
+func faktor(N int32) int32 {
+	var output int32
+	var pembagi int8
+	if N%2 == 0 {
+		pembagi = 2
+	} else {
+		pembagi = 1
+	}
+	if N <= 0 {
+		output = 1
+	} else {
+		output = N / int32(pembagi) * faktor(N-1)
+	}
+
+	return output
+}
+
+func gambar(N int8) {
+	var i int8
+	if N > 0 {
+		gambar(N - 1)
+		for i = 1; i <= N; i++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+		gambar(N - 1)
+	}
 }
